5c303f3b7416e2aff52ebeff7415c25e4d2aafb0
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2024-12-31 00:11:26 +0200

    void methods

---
+++ practice/void_function.java
@@ -0,0 +1,29 @@
+/*
+Write a Java program with two void methods:
+
+printAnimalFact: This method does the following:
+Takes a string parameter (the animal's name).
+Prints a fun fact about the given animal.
+main Method:
+Calls printAnimalFact with at least two different animal names.
+
+
+*/
+
+public class MyClass {
+    
+    public static void Printfact(String dog, String cat){
+        System.out.println("Did you know? Dogs can hear sounds up to 4 times farther than humans.");
+        System.out.println("Did you know? Cats spend 70% of their lives sleeping.");
+    }
+    
+    
+  public static void main(String args[]) {
+  String dog = "dog";
+  String cat = "cat";
+ 
+printfact(dog,  cat);
+  
+
+  }
+}
