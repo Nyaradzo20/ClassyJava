7c5acc415c12179c575ca7bd4248aec5dadad87c
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2025-01-22 13:26:06 +0200

    nested loop

---
+++ practice/pyramid.java
@@ -0,0 +1,17 @@
+public class pyramid{
+    public static void main(String[] args){
+        //make 6 rows
+         for(int rows = 0; rows<= 6; ++rows){
+             //execute as long as j isx smaller or equals  to the number of rows
+               for (int j = 1; j <= rows; ++j) 
+
+             {
+                 System.out.print("*");
+
+                 
+             }
+             //print lines between rows
+                 System.out.println(" ");
+}
+}
+}
