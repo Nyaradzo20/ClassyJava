fed77aee64acf5dd04db1b7ad14dd2d386c9cfe5
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2025-01-29 01:38:43 +0200

    create +print array

---
+++ practice/Array1.java
@@ -0,0 +1,21 @@
+public class Array1 {
+    public static void main(String[] args) {
+       
+        double[]  numbers =  {10, 20, 30, 40 ,50} ;   
+        int i = 0;
+      
+      for(  i = 0; i <= numbers.length -1; i++){
+          System.out.print(numbers[i]);
+          if(i  < numbers.length-1){
+        System.out.print(",");
+          
+    }
+      }
+      
+}
+}
+
+         
+
+        
+    
