9456c6a660bca1dab6fac62e4c102e9ab305b4e9
Nyaradzo Chinyati <nchinyati20@gmail.com>
2025-02-20 00:19:30 +0200

    Add BankAccount class for synchronized withdrawals and update launch configuration

---
+++ practice/threads/BankAccount.java
@@ -0,0 +1,29 @@
+package threads;
+
+public class BankAccount implements Runnable {
+    private double balance = 1000;
+
+    public synchronized void withdraw(double amount) {
+        System.out.println(Thread.currentThread().getName() + " is trying to withdraw: $" + amount);
+        if (amount > balance) {
+            System.out.println("Not enough funds for " + Thread.currentThread().getName());
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful for " + Thread.currentThread().getName() + ". New balance: $" + balance);
+        }
+    }
+
+    @Override
+    public void run() {
+        withdraw(700.00);
+    }
+
+    public static void main(String[] args) {
+        BankAccount sharedAccount = new BankAccount();
+        Thread Alice = new Thread(sharedAccount, "Alice");
+        Thread Bob = new Thread(sharedAccount, "Bob");
+
+        Alice.start();
+        Bob.start();
+    }
+}
